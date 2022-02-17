b6bbf09227ce8494242e20cb0b521aa8a9096358
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-02-17 10:08:54 +0000

    Fix static id in enable torque function

--- coursework/enableDynamixelTorque.m
+++ coursework/enableDynamixelTorque.m
@@ -1,7 +1,7 @@
 function enableDynamixelTorque(dxl_id, port_num, PROTOCOL_VERSION, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE, COMM_SUCCESS)
 %DISABLEDYNAMIXELTORQUE Enable Dynamixel torque.
 
-    write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE);
+    write1ByteTxRx(port_num, PROTOCOL_VERSION, dxl_id, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE);
     dxl_comm_result = getLastTxRxResult(port_num, PROTOCOL_VERSION);
     dxl_error = getLastRxPacketError(port_num, PROTOCOL_VERSION);
     if dxl_comm_result ~= COMM_SUCCESS
