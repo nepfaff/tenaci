b82a5c34612aaaf90644bef70143cb8ff0a06c1a
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-02-21 23:02:37 +0000

    Add function for computing joint space set points using task space cubic interpolation

---
+++ coursework/TaskSpaceTrajectorySetPoints.m
@@ -0,0 +1,33 @@
+function [jointAngles, times] = TaskSpaceTrajectorySetPoints(startWaypoint, endWaypoint, timeForTrajectory, samplePeriod)
+%TASKSPACETRAJECTORYSETPOINTS Returns samples of a cubic task space
+%interpolation in joint space representation.
+    
+    startToolPose = waypointToPoseVector(startWaypoint);
+    endToolPose = waypointToPoseVector(endWaypoint);
+    
+    toolPoseDelta = endToolPose - startToolPose;
+    
+    % Polynomical coefficients
+    a0 = startToolPose;
+    a1 = 0;
+    a2 = 3 / (timeForTrajectory^2) * toolPoseDelta;
+    a3 = -2 / (timeForTrajectory^3) * toolPoseDelta;
+    
+    % Sample polynomial
+    jointAngles = [];
+    times = [];
+    for t = 0 : samplePeriod : timeForTrajectory
+        tool_pose = a0 + a1*t + a2*t^2 + a3*t^3;
+        
+        % Covert into joint space
+        ik_sols = OpenManipIK(tool_pose(1), tool_pose(2), tool_pose(3), tool_pose(4));
+        ik_sol = getFirstValidIKSol(ik_sols);
+        
+        jointAngles = [jointAngles, ik_sol];
+        times = [times, t];
+    end
+end
+
+function toolPose = waypointToPoseVector(waypoint)
+    toolPose = [waypoint.x, waypoint.y, waypoint.z, waypoint.theta];
+end
