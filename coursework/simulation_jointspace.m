c6a296cb6e2f2c18512bace60c89996e2ba2cd96
jl1719 <jl1719@imperial.ac.uk>
2022-02-25 01:43:12 +0800

    Adding comments

--- coursework/simulation_jointspace.m
+++ coursework/simulation_jointspace.m
@@ -3,12 +3,14 @@ clear;
 close all;
 
 beta = pi/2 - atan(0.024/0.128)
+
 % % Draw a square in y-z plane
 % YZ1 = [0, 0.2, 0.1, -0.523]
 % YZ2 = [0, 0.2, 0.2, -0.523]
 % YZ3 = [0, 0.1, 0.2, -0.523]
 % YZ4 = [0, 0.1, 0.1, -0.523]
 
+%define satrt and end locations here
 startX = [0, 0, 0, 0]
 startY = [0.2, 0.2, 0.1, 0.1]
 startZ = [0.1, 0.2, 0.2, 0.1]
@@ -69,47 +71,38 @@ for i = 1:length(startX)
         z_dir4 = T4(1:3,3)*0.01
         pos4 = T4(:,4)
 
+         % statrt plotting
         hold on
+        % first plot the base frame
         base_x = plot3([0, 0 + x_dir0(1)], [0, 0+ x_dir0(2) ], [0, 0+ x_dir0(3)], 'Color','r','LineWidth',2)
-        hold on
         base_y = plot3([0, 0 + y_dir0(1)], [0, 0 + y_dir0(2)], [0, 0+ y_dir0(3)], 'Color','g','LineWidth',2)
-        hold on
         base_z = plot3([0, 0 + z_dir0(1) ], [0, 0 + z_dir0(2)], [0, 0+ z_dir0(3)], 'Color','b','LineWidth',2)
-        hold on
+        %plot first frame and joint location
         line1 = plot3([0, pos1(1)], [0, pos1(2)], [0,pos1(3)], 'Color','black','LineWidth',2)
-        hold on
         frame1_x = plot3([pos1(1), pos1(1) + x_dir1(1)], [pos1(2), pos1(2)+ x_dir1(2) ], [pos1(3), pos1(3)+ x_dir1(3)], 'Color','r','LineWidth',2)
-        hold on
         frame1_y = plot3([pos1(1), pos1(1) + y_dir1(1)], [pos1(2), pos1(2) + y_dir1(2)], [pos1(3), pos1(3) + y_dir1(3)], 'Color','g','LineWidth',2)
-        hold on
         frame1_z = plot3([pos1(1), pos1(1) + z_dir1(1) ], [pos1(2), pos1(2) + z_dir1(2)], [pos1(3), pos1(3)+ z_dir1(3)], 'Color','b','LineWidth',2)
-        hold on
+       %plot second frame and joint location
         line2 = plot3([pos1(1), pos2(1)], [pos1(2), pos2(2)], [pos1(3),pos2(3)], 'Color','black','LineWidth',2)
-        hold on
         frame2_x = plot3([pos2(1),pos2(1) +  x_dir1(1)], [pos2(2), pos2(2) +  x_dir1(2)], [pos2(3), pos2(3)+  x_dir1(3)], 'Color','r','LineWidth',2)
-        hold on
         frame2_y = plot3([pos2(1), pos2(1) +  y_dir2(1)], [pos2(2), pos2(2) + y_dir2(2)], [pos2(3), pos2(3)+ y_dir2(3)], 'Color','g','LineWidth',2)
-        hold on
         frame2_z = plot3([pos2(1),pos2(1) + z_dir2(1) ], [pos2(2), pos2(2) + z_dir2(2)], [pos2(3), pos2(3)+ z_dir2(3)], 'Color','b','LineWidth',2)
-        hold on
+      %plot third frame and joint location
         line3 =plot3([pos2(1), pos3(1)], [pos2(2), pos3(2)], [pos2(3), pos3(3)], 'Color','black','LineWidth',2)
-        hold on
         frame3_x = plot3([pos3(1),pos3(1) +  x_dir3(1)], [pos3(2), pos3(2) +  x_dir3(2)], [pos3(3), pos3(3)+  x_dir3(3)], 'Color','r','LineWidth',2)
-        hold on
         frame3_y = plot3([pos3(1), pos3(1) +  y_dir3(1)], [pos3(2), pos3(2)+ y_dir3(2)], [pos3(3), pos3(3)+ y_dir3(3)], 'Color','g','LineWidth',2)
-        hold on
         frame3_z = plot3([pos3(1), pos3(1) + z_dir3(1) ], [pos3(2),pos3(2) + z_dir3(2)], [pos3(3), pos3(3)+ z_dir3(3)], 'Color','b','LineWidth',2)
-        hold on
+     %plot forth frame and end tool location
         line4 =plot3([pos3(1), pos4(1)], [pos3(2), pos4(2)], [pos3(3), pos4(3)], 'Color','black','LineWidth',2)
-        hold on
         frame4_x = plot3([pos4(1), pos4(1) +  x_dir4(1)], [pos4(2), pos4(2) +  x_dir4(2)], [pos4(3), pos4(3)+  x_dir4(3)], 'Color','r','LineWidth',2)
-        hold on
         frame4_y = plot3([pos4(1), pos4(1) +  y_dir4(1)], [pos4(2), pos4(2) + y_dir4(2)], [pos4(3), pos4(3)+ y_dir4(3)], 'Color','g','LineWidth',2)
-        hold on
         frame4_z = plot3([pos4(1), pos4(1) + z_dir4(1) ], [pos4(2), pos4(2) + z_dir4(2)], [pos4(3), pos4(3)+ z_dir4(3)], 'Color','b','LineWidth',2)
-        hold on
+      % mark the end tool position (leave a trace)
         plot3( pos4(1), pos4(2), pos4(3), '.', 'markersize', 8)
         pause(0.001)
+        
+        % Unless the trajactory stop, otherwise keep deleting the old frame
+       % and link and plot the new links and frames
         if( (i == length(startX)) && (j == length(times))) 
             break
         else 
--- coursework/simulation_taskspace.m
+++ coursework/simulation_taskspace.m
@@ -40,7 +40,7 @@ ylabel('Y')
 zlabel('Z')
 xlim([-0.21, 0.21])
 ylim([-0.21, 0.21])
-zlim([0 0.457])
+zlim([0 0.42])
 title("Forward Kinematics")
 grid on
 hold on
@@ -58,89 +58,79 @@ for i = 1:length(waypoints)
     thetas3 = []
     thetas4 = []
     for j = 1:length(times)
-        
         %calculate thetas(1,2 3,4) for each trajactory
         thetas1 =  [thetas1, wayangle(j).joint1_angle]
         thetas2 =  [thetas2, wayangle(j).joint2_angle]
         thetas3 =  [thetas3, wayangle(j).joint3_angle]
         thetas4 =  [thetas4, wayangle(j).joint4_angle]
         
-       % base frame
+       % define base frame
         T0 = tfFromDH(0.0, 0.0, 0.0, thetas1(j))
         x_dir0 = T0(1:3,1)*0.01
         y_dir0 = T0(1:3,2)*0.01
         z_dir0 = T0(1:3,3)*0.01
         pos0 = T0(:,4)
 
-       % first joint
+       % define first joint and frame loation 
         T1 = T0* tfFromDH(0, 0.0, 0.077,pi/2);
         x_dir1 = T1(1:3,1)*0.01
         y_dir1 = T1(1:3,2)*0.01
         z_dir1 = T1(1:3,3)*0.01
         pos1 = T1(:,4)
 
-       % second joint
+       % define second joint and frame location
         T2 = T1 * tfFromDH(pi/2, 0.0, 0.0, -thetas2(j) + beta)* tfFromDH(0.0, 0.130, 0, -thetas3(j)-beta);
         x_dir2 = T2(1:3,1)*0.01
         y_dir2 = T2(1:3,2)*0.01
         z_dir2 = T2(1:3,3)*0.01
         pos2 = T2(:,4)
 
-        %third joint
+        %define third joint and frame location
         T3 = T2 * tfFromDH(0, 0.124, 0.0, -thetas4(j))
         x_dir3 = T3(1:3,1)*0.01
         y_dir3 = T3(1:3,2)*0.01
         z_dir3 = T3(1:3,3)*0.01
         pos3 = T3(:,4)
 
-       % forth joint
+       % define third joint and frame location
         T4 = T3 * tfFromDH(0, 0.126, 0.0, 0)
         x_dir4 = T4(1:3,1)*0.01
         y_dir4 = T4(1:3,2)*0.01
         z_dir4 = T4(1:3,3)*0.01
         pos4 = T4(:,4)
-
+        
+        % statrt plotting
         hold on
+        % first plot the base frame
         base_x = plot3([0, 0 + x_dir0(1)], [0, 0+ x_dir0(2) ], [0, 0+ x_dir0(3)], 'Color','r','LineWidth',2)
-        hold on
         base_y = plot3([0, 0 + y_dir0(1)], [0, 0 + y_dir0(2)], [0, 0+ y_dir0(3)], 'Color','g','LineWidth',2)
-        hold on
         base_z = plot3([0, 0 + z_dir0(1) ], [0, 0 + z_dir0(2)], [0, 0+ z_dir0(3)], 'Color','b','LineWidth',2)
-        hold on
+        %plot first frame and joint location
         line1 = plot3([0, pos1(1)], [0, pos1(2)], [0,pos1(3)], 'Color','black','LineWidth',2)
-        hold on
         frame1_x = plot3([pos1(1), pos1(1) + x_dir1(1)], [pos1(2), pos1(2)+ x_dir1(2) ], [pos1(3), pos1(3)+ x_dir1(3)], 'Color','r','LineWidth',2)
-        hold on
         frame1_y = plot3([pos1(1), pos1(1) + y_dir1(1)], [pos1(2), pos1(2) + y_dir1(2)], [pos1(3), pos1(3) + y_dir1(3)], 'Color','g','LineWidth',2)
-        hold on
         frame1_z = plot3([pos1(1), pos1(1) + z_dir1(1) ], [pos1(2), pos1(2) + z_dir1(2)], [pos1(3), pos1(3)+ z_dir1(3)], 'Color','b','LineWidth',2)
-        hold on
+       %plot second frame and joint location
         line2 = plot3([pos1(1), pos2(1)], [pos1(2), pos2(2)], [pos1(3),pos2(3)], 'Color','black','LineWidth',2)
-        hold on
         frame2_x = plot3([pos2(1),pos2(1) +  x_dir1(1)], [pos2(2), pos2(2) +  x_dir1(2)], [pos2(3), pos2(3)+  x_dir1(3)], 'Color','r','LineWidth',2)
-        hold on
         frame2_y = plot3([pos2(1), pos2(1) +  y_dir2(1)], [pos2(2), pos2(2) + y_dir2(2)], [pos2(3), pos2(3)+ y_dir2(3)], 'Color','g','LineWidth',2)
-        hold on
         frame2_z = plot3([pos2(1),pos2(1) + z_dir2(1) ], [pos2(2), pos2(2) + z_dir2(2)], [pos2(3), pos2(3)+ z_dir2(3)], 'Color','b','LineWidth',2)
-        hold on
+      %plot third frame and joint location
         line3 =plot3([pos2(1), pos3(1)], [pos2(2), pos3(2)], [pos2(3), pos3(3)], 'Color','black','LineWidth',2)
-        hold on
         frame3_x = plot3([pos3(1),pos3(1) +  x_dir3(1)], [pos3(2), pos3(2) +  x_dir3(2)], [pos3(3), pos3(3)+  x_dir3(3)], 'Color','r','LineWidth',2)
-        hold on
         frame3_y = plot3([pos3(1), pos3(1) +  y_dir3(1)], [pos3(2), pos3(2)+ y_dir3(2)], [pos3(3), pos3(3)+ y_dir3(3)], 'Color','g','LineWidth',2)
-        hold on
         frame3_z = plot3([pos3(1), pos3(1) + z_dir3(1) ], [pos3(2),pos3(2) + z_dir3(2)], [pos3(3), pos3(3)+ z_dir3(3)], 'Color','b','LineWidth',2)
-        hold on
+     %plot forth frame and end tool location
         line4 =plot3([pos3(1), pos4(1)], [pos3(2), pos4(2)], [pos3(3), pos4(3)], 'Color','black','LineWidth',2)
-        hold on
         frame4_x = plot3([pos4(1), pos4(1) +  x_dir4(1)], [pos4(2), pos4(2) +  x_dir4(2)], [pos4(3), pos4(3)+  x_dir4(3)], 'Color','r','LineWidth',2)
-        hold on
         frame4_y = plot3([pos4(1), pos4(1) +  y_dir4(1)], [pos4(2), pos4(2) + y_dir4(2)], [pos4(3), pos4(3)+ y_dir4(3)], 'Color','g','LineWidth',2)
-        hold on
         frame4_z = plot3([pos4(1), pos4(1) + z_dir4(1) ], [pos4(2), pos4(2) + z_dir4(2)], [pos4(3), pos4(3)+ z_dir4(3)], 'Color','b','LineWidth',2)
-        hold on
+      % mark the end tool position (leave a trace)
         plot3( pos4(1), pos4(2), pos4(3), '.', 'markersize', 8)
         pause(0.001)
+      
+        % Unless the trajactory stop, otherwise keep deleting the old frame
+       % and link and plot the new links and frames
         if( (i == length(waypoints)) && (j == length(times))) 
             break
         else 
