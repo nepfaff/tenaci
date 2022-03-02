9074945411f30a6634550f678ab011d7bae806fc
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-02 16:45:27 +0000

    Factor out common helper function from simulation scripts

--- coursework/simulation/simulation_jointspace.m
+++ coursework/simulation/simulation_jointspace.m
@@ -133,15 +133,3 @@ function plot = jointspace_sim(startX, startY, startZ, startTheta, endX, endY, e
     end
 end
 
-function tf = tfFromDH(alpha, a, d, theta)
-%tfFromDH Converts DH parameters into a homogenous transformation matrix.
-
-    tf = [
-        cos(theta), -sin(theta), 0, a;
-        sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d;
-        sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha)*d;
-        0 0 0 1
-    ];
-end
-
-
--- coursework/simulation/simulation_taskspace.m
+++ coursework/simulation/simulation_taskspace.m
@@ -177,17 +177,3 @@ function plot = taskspance_sim(waypoints, timeForTrajectory, samplePeriod)
         end
     end
 end
-
-
-function tf = tfFromDH(alpha, a, d, theta)
-%tfFromDH Converts DH parameters into a homogenous transformation matrix.
-
-    tf = [
-        cos(theta), -sin(theta), 0, a;
-        sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d;
-        sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha)*d;
-        0 0 0 1
-    ];
-end
-
-
