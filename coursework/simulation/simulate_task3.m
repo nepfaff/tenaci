7f3bb741a3b17911e9ed508c5fae1991063e64ed
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-03 11:06:14 +0000

    Add valid lines to task 3 simulation

--- coursework/simulation/simulate_task3.m
+++ coursework/simulation/simulate_task3.m
@@ -22,13 +22,16 @@ startPose.groupToPrevious = false;
 [startLocations, endLocations] = getTask2CubeLocations();
 
 % Task specific waypoints
-line1Start.x = 0.15;
-line1Start.y = 0.0;
+line1Start.x = -0.05;
+line1Start.y = 0.2;
 line1Start.z = 0.05;
-line1End.x = 0.0;
-line1End.y = 0.15;
+line1End.x = 0.05;
+line1End.y = 0.2;
 line1End.z = 0.05;
-waypoints = [line1Start, line1End];
+line2End.x = 0.10;
+line2End.y = 0.18;
+line2End.z = 0.05;
+waypoints = [line1Start, line1End, line2End];
 
 % Constant for drawing task
 for i = 1 : length(waypoints)
@@ -36,6 +39,19 @@ for i = 1 : length(waypoints)
    waypoints(i).gripper = GRIPPER_CUBE_HOLD_POS;
    waypoints(i).groupToPrevious = false; % This should be true for circles
 end
+
+% Draw arc
+% arc = drawArc([-0.05, 0.2, 0.05], [0.010, 0.1, 0.05], 10, 1);
+% waypoints = [];
+% for i = 1 : length(arc)
+%     waypoint.x = arc(i,1);
+%     waypoint.y = arc(i,2);
+%     waypoint.z = arc(i,3);
+%     waypoint.theta = 0.0;
+%     waypoint.gripper = GRIPPER_CUBE_HOLD_POS;
+%     waypoint.groupToPrevious = true;
+%     waypoints = [waypoints, waypoint];
+% end
 
 waypoints = [startPose, waypoints];
 
