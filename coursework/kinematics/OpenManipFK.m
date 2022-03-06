b8c84f3ee7404b035a16c17840849fc057f6cffe
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-06 19:59:27 +0000

    Remove now redundant tfFromDH helper function

--- coursework/kinematics/OpenManipFK.m
+++ coursework/kinematics/OpenManipFK.m
@@ -19,14 +19,3 @@ function [x, y, z] = OpenManipFK(joint1_angle, joint2_angle, joint3_angle, joint
     y = tool_position(2);
     z = tool_position(3);
 end
-
-function tf = tfFromDH(alpha, a, d, theta)
-%tfFromDH Converts DH parameters into a homogenous transformation matrix.
-
-    tf = [
-        cos(theta), -sin(theta), 0, a;
-        sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d;
-        sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha)*d;
-        0 0 0 1
-    ];
-end
