5591bfa9668256759da038a11a2c9da2ede534c7
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-08 15:22:09 +0000

    Adjust task 2 gripper z-values

--- coursework/config/getTask2GripperZValues.m
+++ coursework/config/getTask2GripperZValues.m
@@ -8,15 +8,15 @@ function...
     % Cube specific values
     % Cube has sidelength of 2.5cm, cube stand's top edge is 19.8mm from
     % base-plate
-    GRIPPER_Z_PICK_UP_CUBE_FACING_DOWN = 0.057;
-    GRIPPER_Z_ABOVE_CUBE_PICK_UP_FACING_DOWN = 0.075;
+    GRIPPER_Z_PICK_UP_CUBE_FACING_DOWN = 0.065;
+    GRIPPER_Z_ABOVE_CUBE_PICK_UP_FACING_DOWN = 0.085;
     
-    GRIPPER_Z_PICK_UP_CUBE_FACING_STRAIGHT = 0.04;
+    GRIPPER_Z_PICK_UP_CUBE_FACING_STRAIGHT = 0.048;
     GRIPPER_Z_ABOVE_CUBE_PICK_UP_FACING_STRAIGHT = 0.07;
     
     % Gripper can't pick cube in the middle when picking facing down
     % This causes problems when for example picking up a cube facing down
     % and placing it facing straight. The placed cube won't be at the
     % desired destination due to the offset
-    GRIPPER_PICK_DOWN_OFFSET = 0.013;
+    GRIPPER_PICK_DOWN_OFFSET = 0.011;
 end
