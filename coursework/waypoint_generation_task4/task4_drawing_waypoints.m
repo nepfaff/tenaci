5bef9281f29c5ef0fddc9b9878892c99f8101fa1
jl1719 <jl1719@imperial.ac.uk>
2022-03-17 20:35:15 +0800

    task4_waypoints1

--- coursework/waypoint_generation_task4/task4_drawing_waypoints.m
+++ coursework/waypoint_generation_task4/task4_drawing_waypoints.m
@@ -38,37 +38,37 @@ function waypoints = task4_drawing_waypoints(...
 
     
     % Drawing Letter E
-    E_line1Above.x = 0.18;
-    E_line1Above.y = 0.155;
-    E_line1Above.z = drawingHeight + 0.02;
-    E_line1Above.timeForTrajectory = 0.3;
-    E_line1Above.name = "Above E start";
+    N_line1Above.x = 0.18;
+    N_line1Above.y = 0.155;
+    N_line1Above.z = drawingHeight + 0.02;
+    N_line1Above.timeForTrajectory = 0.3;
+    N_line1Above.name = "Above E start";
 
-    E_line1Start.x = 0.18;
-    E_line1Start.y = 0.155;
-    E_line1Start.z = drawingHeight;
-    E_line1Start.timeForTrajectory = 0.1;
-    E_line1Start.name = "E start";
-    E_line1End.x = 0.2;
-    E_line1End.y = 0.155;
-    E_line1End.z = drawingHeight;
-    E_line1End.timeForTrajectory = 0.5;
-    E_line1End.name = "Horizontal line end";
-    E_line2End.x = 0.2;
-    E_line2End.y = 0.195;
-    E_line2End.z = drawingHeight;
-    E_line2End.timeForTrajectory = 0.8;
-    E_line2End.name = "Vertical line end";
-    E_line3End.x = 0.18;
-    E_line3End.y = 0.195;
-    E_line3End.z = drawingHeight;
-    E_line3End.timeForTrajectory = 0.5;
-    E_line3End.name = "Horizontal line end";
-    E_line3AboveEnd.x = 0.18;
-    E_line3AboveEnd.y = 0.195;
-    E_line3AboveEnd.z = drawingHeight + 0.02;
-    E_line3AboveEnd.timeForTrajectory = 0.2;
-    E_line3AboveEnd.name = "Horizontal line end above";
+    N_line1Start.x = 0.18;
+    N_line1Start.y = 0.155;
+    N_line1Start.z = drawingHeight;
+    N_line1Start.timeForTrajectory = 0.1;
+    N_line1Start.name = "E start";
+    N_line1End.x = 0.2;
+    N_line1End.y = 0.155;
+    N_line1End.z = drawingHeight;
+    N_line1End.timeForTrajectory = 0.5;
+    N_line1End.name = "Horizontal line end";
+    N_line2End.x = 0.2;
+    N_line2End.y = 0.195;
+    N_line2End.z = drawingHeight;
+    N_line2End.timeForTrajectory = 0.8;
+    N_line2End.name = "Vertical line end";
+    N_line3End.x = 0.18;
+    N_line3End.y = 0.195;
+    N_line3End.z = drawingHeight;
+    N_line3End.timeForTrajectory = 0.5;
+    N_line3End.name = "Horizontal line end";
+    N_line3AboveEnd.x = 0.18;
+    N_line3AboveEnd.y = 0.195;
+    N_line3AboveEnd.z = drawingHeight + 0.02;
+    N_line3AboveEnd.timeForTrajectory = 0.2;
+    N_line3AboveEnd.name = "Horizontal line end above";
 
     E_line4Above.x = 0.2;
     E_line4Above.y = 0.175;
@@ -91,24 +91,62 @@ function waypoints = task4_drawing_waypoints(...
     E_line4EndAbove.timeForTrajectory = 0.35;
     E_line4EndAbove.name = "Horizontal end";
     
-    E_lineWaypoints = [E_line1Above, E_line1Start, E_line1End, E_line2End, E_line3End, ...
-                       E_line3AboveEnd, E_line4Above,E_line4Start, E_line4End, ...
+    E_lineWaypoints = [N_line1Above, N_line1Start, N_line1End, N_line2End, N_line3End, ...
+                       N_line3AboveEnd, E_line4Above,E_line4Start, E_line4End, ...
                        E_line4EndAbove];
 
+   % Drawing Letter N
+   
+    N_line1Above.x = 0.17;
+    N_line1Above.y = 0.195;
+    N_line1Above.z = drawingHeight + 0.02;
+    N_line1Above.timeForTrajectory = 0.3;
+    N_line1Above.name = "Above E start";
+
+    N_line1Start.x = 0.17;
+    N_line1Start.y = 0.195;
+    N_line1Start.z = drawingHeight;
+    N_line1Start.timeForTrajectory = 0.1;
+    N_line1Start.name = "N start";
+    N_line1End.x = 0.17;
+    N_line1End.y = 0.155;
+    N_line1End.z = drawingHeight;
+    N_line1End.timeForTrajectory = 0.5;
+    N_line1End.name = "Vertical line end";
+    N_line2End.x = 0.15;
+    N_line2End.y = 0.155;
+    N_line2End.z = drawingHeight;
+    N_line2End.timeForTrajectory = 0.8;
+    N_line2End.name = "Diagnoal line end";
+    N_line3End.x = 0.15;
+    N_line3End.y = 0.155;
+    N_line3End.z = drawingHeight;
+    N_line3End.timeForTrajectory = 0.5;
+    N_line3End.name = "Vertical line end";
+    N_line3AboveEnd.x = 0.15;
+    N_line3AboveEnd.y = 0.155;
+    N_line3AboveEnd.z = drawingHeight + 0.02;
+    N_line3AboveEnd.timeForTrajectory = 0.2;
+    N_line3AboveEnd.name = "Horizontal line end above";
+
+    N_lineWaypoints = [N_line1Above, N_line1Start, N_line1End, N_line2End, N_line3End, N_line3AboveEnd];
+    
+    line_waypoints = [E_lineWaypoints, N_lineWaypoints];
     % Constant for lines
-    for i = 1 : length(E_lineWaypoints)
-       E_lineWaypoints(i).theta = 0.0;
-       E_lineWaypoints(i).gripper = gripperPenCubeHoldPos;
+    for i = 1 : length(line_waypoints)
+       line_waypoints(i).theta = 0.0;
+       line_waypoints(i).gripper = gripperPenCubeHoldPos;
        % Lines are more straight if they are defined by start and end waypoints
        % that are not grouped together. Multiple intermediate waypoints for
        % lines works less well.
-       E_lineWaypoints(i).groupToPrevious = false;
+       line_waypoints(i).groupToPrevious = false;
     end
 
     % Combine waypoints
     waypoints = [
        pickUpWaypoints,...
-       E_lineWaypoints
+       E_lineWaypoints,...
+       N_lineWaypoints
     ];
 
 
