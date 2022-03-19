c0aa74f81436577cedb1a6fd86b128898856e8a9
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-19 21:35:21 +0000

    Adjust task 3 times
    Needed to slow down for accuracy. Annoying as it worked before

--- coursework/waypoint_generation_task3/getTask3VideoWaypoints.m
+++ coursework/waypoint_generation_task3/getTask3VideoWaypoints.m
@@ -4,11 +4,11 @@ function waypoints = getTask3VideoWaypoints(...
 %GETTASK3VIDEOWAYPOINTS Returns the waypoints for executing the task 3
 %video specific drawing. The waypoints don't include the start pose.
     
-    drawingHeight = 0.09;
+    drawingHeight = 0.0845;
 
     % Pick up pen
-    penAbovePickUp.x = 0.125;
-    penAbovePickUp.y = 0.125;
+    penAbovePickUp.x = 0.075;
+    penAbovePickUp.y = 0.075;
     penAbovePickUp.z = 0.075;
     penAbovePickUp.theta = -pi/2;
     penAbovePickUp.gripper = gripperOpenPos;
@@ -16,17 +16,17 @@ function waypoints = getTask3VideoWaypoints(...
     penAbovePickUp.timeForTrajectory = 0.4;
     penAbovePickUp.name = "Pen above pick up";
     
-    penPickUp.x = 0.125;
-    penPickUp.y = 0.125;
-    penPickUp.z = 0.02;
+    penPickUp.x = 0.075;
+    penPickUp.y = 0.075;
+    penPickUp.z = 0.015;
     penPickUp.theta = -pi/2;
     penPickUp.gripper = gripperPenCubeHoldPos;
     penPickUp.groupToPrevious = false;
     penPickUp.timeForTrajectory = 0.15;
     penPickUp.name = "Pen pick up";
     
-    penAbovePickUp1.x = 0.125;
-    penAbovePickUp1.y = 0.125;
+    penAbovePickUp1.x = 0.075;
+    penAbovePickUp1.y = 0.075;
     penAbovePickUp1.z = drawingHeight + 0.05;
     penAbovePickUp1.theta = 0.0;
     penAbovePickUp1.gripper = gripperPenCubeHoldPos;
@@ -44,7 +44,7 @@ function waypoints = getTask3VideoWaypoints(...
     line1Above.theta = 0.0;
     line1Above.gripper = gripperPenCubeHoldPos;
     line1Above.groupToPrevious = false;
-    line1Above.timeForTrajectory = 0.3;
+    line1Above.timeForTrajectory = 0.4;
     line1Above.name = "Above line 1 start";
 
     line1Start.x = -0.06;
@@ -55,17 +55,17 @@ function waypoints = getTask3VideoWaypoints(...
     line1End.x = -0.14;
     line1End.y = 0.125;
     line1End.z = drawingHeight;
-    line1End.timeForTrajectory = 0.35;
+    line1End.timeForTrajectory = 0.55;
     line1End.name = "Diagonal line end";
     line2End.x = -0.14;
     line2End.y = 0.2;
     line2End.z = drawingHeight;
-    line2End.timeForTrajectory = 0.3;
+    line2End.timeForTrajectory = 0.4;
     line2End.name = "Vertical line end";
     line3End.x = -0.06;
     line3End.y = 0.2;
     line3End.z = drawingHeight;
-    line3End.timeForTrajectory = 0.3;
+    line3End.timeForTrajectory = 0.5;
     line3End.name = "Horizontal line end";
     lineWaypoints = [line1Start, line1End, line2End, line3End];
     % Constant for lines
@@ -87,7 +87,7 @@ function waypoints = getTask3VideoWaypoints(...
         arcWaypoints(i).gripper = gripperPenCubeHoldPos;
         % Directly group to last line point as otherwise we have a delay before starting arc
         arcWaypoints(i).groupToPrevious = true;
-        arcWaypoints(i).timeForTrajectory = 1;
+        arcWaypoints(i).timeForTrajectory = 2;
         arcWaypoints(i).name = "arc";
     end
 
