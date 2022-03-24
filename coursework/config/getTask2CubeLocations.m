6bcc603409f18fdc730c1a48513b36ea810a15dd
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-24 09:20:59 +0000

    Add demo cube locations

--- coursework/config/getTask2CubeLocations.m
+++ coursework/config/getTask2CubeLocations.m
@@ -1,27 +1,27 @@
 function [startLocations, endLocations] = getTask2CubeLocations()
 %GETTASK2CUBELOCATIONS
 
-    start1.x = 0.15;
-    start1.y = 0.15;
-    start1.direction = "front";
+    start1.x = 0.175;
+    start1.y = 0.175;
+    start1.direction = "back";
     
-    start2.x = 0.2;
-    start2.y = 0.075;
+    start2.x = -0.05;
+    start2.y = 0.175;
     start2.direction = "down";
     
-    start3.x = -0.15;
-    start3.y = 0.15;
-    start3.direction = "back";
+    start3.x = -0.175;
+    start3.y = 0.0;
+    start3.direction = "front";
     
-    finish1.x = -0.2;
+    finish1.x = 0.15;
     finish1.y = 0.0;
     
-    finish2.x = 0.0;
-    finish2.y = 0.075;
+    finish2.x = 0.1;
+    finish2.y = 0.1;
     
-    finish3.x = 0.125;
-    finish3.y = 0.0;
+    finish3.x = 0.0;
+    finish3.y = 0.225;
     
-    startLocations = [start1, start2, start3];
+    startLocations = [start3, start2, start1];
     endLocations = [finish1, finish2, finish3];
 end
